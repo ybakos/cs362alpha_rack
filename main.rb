ef8903e9579d134c19d88074377485b4cbbe0ea8
Tim Leavey <timothy.leavey@gmail.com>
2017-02-08 14:48:16 -0800

    Added the class Album to main.rb

--- main.rb
+++ main.rb
@@ -1,18 +1,33 @@
 require 'rack'
 
+
+class  Album
+
+  attr_accessor
+  attr_reader(:rank, :title, :year)
+
+  def initialize(rank, title, year)
+    @rank = rank
+    @title = title
+    @year = year
+  end
+
+end
+
+
 class TopAlbumsApp
 
-	def call(env) 
-		albums = []
-		File.open("top_100_albums.txt", "r") do |file|
-			albums = file.readlines
-		end
-		['200', {'Content-Type' => 'text/html'}, [albums.to_s]]
-	end
-	#design pattern front controller for call method
+  def call(env) 
+    albums = []
+    File.open("top_100_albums.txt", "r") do |file|
+      albums = file.readlines
+    end
+
+    ['200', {'Content-Type' => 'text/html'}, [albums.to_s]]
+  end
+  #design pattern front controller for call method
 end
 
 Rack::Handler::WEBrick.run(TopAlbumsApp.new)
-
 
 
