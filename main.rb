497b599b2a6b3ab6ae0465de176917797c57e001
Tim Leavey <timothy.leavey@gmail.com>
2017-02-11 12:34:43 -0800

    Fixes #14. Created initializer for our top apps class.

--- main.rb
+++ main.rb
@@ -14,33 +14,31 @@ class  Album
 
 end
 
-allOfTheAlbums = Array.new
-
-def makeAlbums(array)
-
-  albums.map.with_index do |i, j| 
-    album = Album.new
-    albumProperties = albums.split(',')
-    album.rank = j + 1
-    album.title = albumProperties[0]
-    album.year = albumProperties[1]
-    allOfTheAlbums << album
-
-    end
-
-
 class TopAlbumsApp
+  
   include ERB::Util
 
-  def call(env) 
-    albums = []
+  def initialize
+    @allOfTheAlbums = Array.new
     File.open("top_100_albums.txt", "r") do |file|
       albums = file.readlines
     end
+    @allOfTheAlbums = albums.makeAlbums
+  end
 
-    makeAlbums(albums)
-
+  def call(env) 
+    #Placeholder for rendering
     ['200', {'Content-Type' => 'text/html'}, [albums.to_s]]
+  end
+
+  def makeAlbums
+    albums.map.with_index do |i, j| 
+      album = Album.new
+      albumProperties = albums.split(',')
+      album.rank = j + 1
+      album.title = albumProperties[0]
+      album.year = albumProperties[1]
+      allOfTheAlbums << album
   end
   #design pattern front controller for call method
 
