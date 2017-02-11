3eb7a6b345297438bd21504994dceab434d36f13
Tim Leavey <timothy.leavey@gmail.com>
2017-02-11 11:33:29 -0800

    Added render function to top albums class. Still needs a template.

--- config.ru
+++ config.ru
@@ -1,6 +1,12 @@
 require_relative 'main'
+include ERB::Util
 
 use Rack::Reloader, 0
+
+
+def render()
+  ERB.new(@template).result(binding)
+end
 
 run TopAlbumsApp.new
 
--- main.rb
+++ main.rb
@@ -26,6 +26,11 @@ class TopAlbumsApp
     ['200', {'Content-Type' => 'text/html'}, [albums.to_s]]
   end
   #design pattern front controller for call method
+
+  def render()
+    ERB.new(@template).result(binding)
+  end
+
 end
 
 #Rack::Handler::WEBrick.run(TopAlbumsApp.new)
