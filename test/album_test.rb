e64687ebae789449e42db57278f3b532830bc6e9
Jack Wilson <wilsonja@oregonstate.edu>
2017-02-24 16:52:04 -0800

    added test for year and title of album

--- test/album_test.rb
+++ test/album_test.rb
@@ -12,6 +12,12 @@ class AlbumTest < Minitest::Test
     @album = Album.new(RANK, TITLE, YEAR)
   end
 
+  def test_album_constructor
+      assert_equal @album.rank, RANK
+      assert_equal @album.title, TITLE
+      assert_equal @album.year, YEAR
+  end  
+
   def test_album_rank_is_greater_than_zero
     assert(@album.rank > 0)
   end
@@ -20,14 +26,13 @@ class AlbumTest < Minitest::Test
     assert(@album.rank < 101)
   end
 
-  def test_album_rank_is_not_empty
-    flunk
+  def test_album_title_is_not_empty
+    assert(!@album.title.empty?)
   end
 
-  def test_album_constructor
-      assert_equal @album.rank, RANK
-      assert_equal @album.title, TITLE
-      assert_equal @album.year, YEAR
+  def test_album_year_is_not_greater_than_current_year
+    current_year = Time.new.year.to_i
+    assert(@album.year.to_i <= current_year)
   end
 
 end
