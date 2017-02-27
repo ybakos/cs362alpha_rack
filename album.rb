245ea713a42c212aed1d8f2e7a487e7521e662b8
Jack Wilson <wilsonja@oregonstate.edu>
2017-02-27 13:14:17 -0800

    used assert raises to test for failing condition

--- album.rb
+++ album.rb
@@ -5,6 +5,14 @@ class Album
   attr_reader :rank, :title, :year 
 
   def initialize(rank, title, year)
+  	raise ArgumentError.new("Title cannot be empty") if title.empty?
+  	raise ArgumentError.new("Rank cannot be a non integer") if rank.to_i.is_a?(String)
+    raise ArgumentError.new("Rank cannot be less than 1") if rank.to_i < 1
+    raise ArgumentError.new("Rank cannot be greater than 100") if rank.to_i > 100
+
+    current_year = Time.new.year.to_i
+    raise ArgumentError.new("Year cannot be greater than the current year") if year.to_i > current_year
+
     @rank = rank
     @title = title
     @year = year
--- test/album_test.rb
+++ test/album_test.rb
@@ -1,8 +1,7 @@
 require 'minitest/autorun'
-
 require_relative '../album'
 
-RANK = 1
+RANK = "1"
 TITLE = "Test Album"
 YEAR = "2017"
 
@@ -19,24 +18,53 @@ class AlbumTest < Minitest::Test
   end  
 
   def test_album_rank_is_integer
-    assert(@album.rank.is_a?(Integer) == true)
+    assert_raises ArgumentError do
+      rank = "wrong"
+      title = "testing"
+      year = "1999"
+
+      testAlbum = Album.new(rank, title, year)
+    end
   end
 
   def test_album_rank_is_greater_than_zero
-    assert(@album.rank > 0)
+    assert_raises ArgumentError do
+      rank = "-11"
+      title = "testing"
+      year = "1999"
+      
+      testAlbum = Album.new(rank, title, year)
+    end
   end
 
   def test_album_rank_is_less_than_101
-    assert(@album.rank < 101)
+    assert_raises ArgumentError do
+      rank = "105"
+      title = "testing"
+      year = "1999"
+
+      testAlbum = Album.new(rank, title, year)
+    end
   end
 
   def test_album_title_is_not_empty
-    assert(!@album.title.empty?)
+      assert_raises ArgumentError do
+      rank = "1"
+      title = ""
+      year = "1999"
+
+      testAlbum = Album.new(rank, title, year)
+    end
   end
 
   def test_album_year_is_not_greater_than_current_year
-    current_year = Time.new.year.to_i
-    assert(@album.year.to_i <= current_year)
+    assert_raises ArgumentError do
+      rank = "1"
+      title = "Title"
+      year = "2800"
+
+      testAlbum = Album.new(rank, title, year)
+    end
   end
 
 end
